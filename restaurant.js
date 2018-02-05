0ffaa850c3c5a6ff703278deeaf75085ae75b80f
Sniedze <u.sniedze@gmail.com>
2018-02-05 08:41:31 +0100

    Connected outside data in JS

--- restaurant.js
+++ restaurant.js
@@ -1,12 +1,21 @@
 let mylink = "http://kea-alt-del.dk/t5/api/productlist";
-let imagePath = "http://kea-alt-del.dk/t5/site/imgs/small";
-let main = document.querySelector("main");
-let template = document.querySelector(".course");
+let imagePath = "http://kea-alt-del.dk/t5/site/imgs/small/";
+const main = document.querySelector("#menu");
+const template = document.querySelector(".course");
 
 function loadData(link) {
     fetch(link).then(e => e.json()).then(data => show(data));
 }
 
 function show(data) {
-    data.forEach(object => console.log(object));
+    data.forEach(element => {
+        let clone = template.cloneNode(true).content;
+        clone.querySelector(".product-small-image").src = imagePath + element.image + "-sm.jpg";
+        clone.querySelector(".name").textContent = element.name;
+        clone.querySelector(".category").textContent = element.category;
+        clone.querySelector(".short-description").textContent = element.shortdescription;
+        clone.querySelector(".price span").textContent = element.price;
+        main.appendChild(clone);
+    });
 }
+loadData(mylink);
