1d2d77af7410302862027a891889c9b3b24f3b09
Sniedze <u.sniedze@gmail.com>
2018-02-05 19:15:35 +0100

    Assignes data to categories

--- restaurant.js
+++ restaurant.js
@@ -1,21 +1,44 @@
-let mylink = "http://kea-alt-del.dk/t5/api/productlist";
-let imagePath = "http://kea-alt-del.dk/t5/site/imgs/small/";
-const main = document.querySelector("#menu");
-const template = document.querySelector(".course");
+"use strict"
+const mylink = "http://kea-alt-del.dk/t5/api/productlist";
+const imagePath = "http://kea-alt-del.dk/t5/site/imgs/";
+const catNames = "http://kea-alt-del.dk/t5/api/categories";
+const main = document.querySelector("main");
+const template = document.querySelector("template").content;
 
-function loadData(link) {
-    fetch(link).then(e => e.json()).then(data => show(data));
+fetch(catNames).then(result => result.json()).then(data => createCatContainers(data));
+
+function createCatContainers(categories) {
+    categories.forEach(category => {
+        const section = document.createElement("section");
+        const h2 = document.createElement("h2");
+        section.id = category;
+        h2.textContent = category;
+        section.appendChild(h2);
+        main.appendChild(section);
+    });
+    fetch(mylink).then(result => result.json()).then(data => show(data));
 }
 
 function show(data) {
     data.forEach(element => {
-        let clone = template.cloneNode(true).content;
-        clone.querySelector(".product-small-image").src = imagePath + element.image + "-sm.jpg";
+        const section = document.querySelector("#" + element.category);
+        const clone = template.cloneNode(true);
+        clone.querySelector("img").src = "http://kea-alt-del.dk/t5/site/imgs/small/" + element.image + "-sm.jpg";
+        if (element.name == "Russisk salatRussian salad") {
+            element.name = "Russian salad";
+
+
+        }
         clone.querySelector(".name").textContent = element.name;
-        clone.querySelector(".category").textContent = element.category;
+
         clone.querySelector(".short-description").textContent = element.shortdescription;
         clone.querySelector(".price span").textContent = element.price;
-        main.appendChild(clone);
-    });
+
+
+        if (element.alcohol) {
+
+            clone.querySelector(".alcohol").textContent = "Contains alcohol " + element.alcohol + "%";
+        }
+        section.appendChild(clone);
+    })
 }
-loadData(mylink);
