0deb0b3b1fc291f9f9b2e4de1032d080b4e9186b
Sniedze <u.sniedze@gmail.com>
2018-02-06 16:12:03 +0100

    fixes spelling mistakes

--- restaurant.js
+++ restaurant.js
@@ -4,6 +4,11 @@ const imagePath = "http://kea-alt-del.dk/t5/site/imgs/";
 const catNames = "http://kea-alt-del.dk/t5/api/categories";
 const main = document.querySelector("main");
 const template = document.querySelector("template").content;
+const nav = document.querySelector("#menu_nav");
+const all = document.querySelector("#menu_nav a");
+
+let h2 = document.querySelector("section h2");
+all.addEventListener("click", () => filter("all"));
 
 fetch(catNames).then(result => result.json()).then(data => createCatContainers(data));
 
@@ -12,12 +17,54 @@ function createCatContainers(categories) {
         const section = document.createElement("section");
         const h2 = document.createElement("h2");
         section.id = category;
+
         h2.textContent = category;
+        if (h2.textContent == "starter") {
+            h2.textContent = "Starters";
+        }
+        if (h2.textContent == "main") {
+            h2.textContent = "Main dishes";
+        }
+        if (h2.textContent == "dessert") {
+            h2.textContent = "desserts";
+        }
+
         section.appendChild(h2);
         main.appendChild(section);
+
+        const a = document.createElement("a");
+        a.textContent = category;
+        if (a.textContent == "starter") {
+            a.textContent = "Starters";
+        }
+        if (a.textContent == "main") {
+            a.textContent = "Main dishes";
+        }
+        if (a.textContent == "dessert") {
+            a.textContent = "desserts";
+        }
+        a.href = "#";
+        a.addEventListener("click", () => filter(category));
+        nav.appendChild(a);
     });
     fetch(mylink).then(result => result.json()).then(data => show(data));
 }
+
+
+function filter(category) {
+
+
+    document.querySelectorAll("main section").forEach(section => {
+
+        if (section.id == category || category == "all") {
+            section.classList.remove("hidden");
+        } else {
+            section.classList.add("hidden");
+
+        }
+    })
+}
+
 
 function show(data) {
     data.forEach(element => {
@@ -26,9 +73,13 @@ function show(data) {
         clone.querySelector("img").src = "http://kea-alt-del.dk/t5/site/imgs/small/" + element.image + "-sm.jpg";
         if (element.name == "Russisk salatRussian salad") {
             element.name = "Russian salad";
-
-
         }
+        if (element.name == "Stake with vegetables") {
+            element.name = "Steak with vegetables";
+        }
+
+
+
         clone.querySelector(".name").textContent = element.name;
 
         clone.querySelector(".short-description").textContent = element.shortdescription;
@@ -40,5 +91,6 @@ function show(data) {
             clone.querySelector(".alcohol").textContent = "Contains alcohol " + element.alcohol + "%";
         }
         section.appendChild(clone);
+
     })
 }
